d9e729df57ab28b19211bedfabbfa904597b18ae
Sabisa <lumisabisa@gmail.com>
2021-04-05 12:46:27 +0200

    robofriends

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name:'Lummy Smith',
+        username:'Lumi',
+        email:'lumi@gmailcom'
+    },
+    {
+        id: 2,
+        name:'Luks Kane',
+        username:'Luks',
+        email:'Lukskane@yahoo.com'
+    },
+    {
+        id: 3,
+        name:'Una Nduk',
+        username:'Una',
+        email:'Unanduk@karo.tv'
+    },
+    {
+        id: 4,
+        name:'Pat Leb',
+        username:'Leb',
+        email:'patleb@hoo.co.za'
+    },
+    {
+        id: 5,
+        name:'Chesley beatrice',
+        username:'beatrice',
+        email:'chesley@ham.org'
+    },
+    {
+        id: 6,
+        name:'Denny Lebben',
+        username:'LDen',
+        email:'Denny@ytrickery.biz'
+    },
+    {
+        id: 7,
+        name:'Max William',
+        username:'William',
+        email:'max@well.mo'
+    },
+        {
+            id: 8,
+            name:'Craig Sim',
+            username:'Sim',
+            email:'craigsim@yols.eo'
+        },
+        {
+            id: 9,
+            name:'Iya Wilson',
+            username:'Iya',
+            email:'Iyawilson@crazy.so'
+        },
+        {
+            id: 10,
+            name:'kingsley Falteni',
+            username:'Kingsly',
+            email:'Kingslyfalteni@willy.etv'
+        }, 
+]
